9c1e32ebc43e421a417dfd544ca9756e6e39e3ac
Chamoouske <ajaxlima94@gmail.com>
2019-12-09 15:36:44 -0300

    Tela update completa

--- js/agenda.js
+++ js/agenda.js
@@ -55,11 +55,9 @@ function agenda_view(){
     db.transaction(agenda_view_db, errorDB, successDB);
 }
 
-
 function agenda_view_db(tx){
     tx.executeSql('SELECT * FROM Agenda', [], agenda_view_data, errorDB);
 }
-
 
 function agenda_view_data(tx, results){
     $('#agenda_listagem').empty();
@@ -69,7 +67,39 @@ function agenda_view_data(tx, results){
         $('#agenda_listagem').append("<tr class='agenda_item_lista'>" + 
             "<td><h3>" + results.rows.item(i).nome + "</h3></td>" + 
             "<td><h3>" + results.rows.item(i).tel + "</h3></td>" +
-            "<td><input type='button' class='btn btn-lg btn-danger' value='X' onclick='agenda_delete(" + results.rows.item(i).id + ")'></td>" +
+            "<td><input type='button' class='btn btn-lg btn-danger' value='X' onclick='agenda_delete(" + results.rows.item(i).id + ")'>" + " " +
+            "<input type='button' class='btn btn-lg btn-warning' value='E' onclick='agenda_update_abrir_tela(" + results.rows.item(i).id + ")'></td>" +
         "</tr>");
     }
+}
+
+function agenda_update_abrir_tela(agenda_id){
+    $("#tela_padrao").hide(); // esconde tela inicial
+    $("#tela_edicao").show(); // Mostra tela de edicao
+
+    var agenda_nome_update = $("#agenda_item_" + agenda_id + ".agenda_info h3").html();
+    var agenda_telefone_update = $("#agenda_item_" + agenda_id + ".agenda_info h5").html();
+
+    $("#agenda_id_update").val(agenda_id);
+    $("#agenda_nome_update").val(agenda_nome_update);
+    $("#agenda_telefone_update").val(agenda_telefone_update);
+}
+
+function agenda_update_fechar_tela(){
+    $("#tela_padrao").show(); // Mostra tela inicial
+    $("#tela_edicao").hide(); // esconde tela de edicao
+}
+
+function agenda_update(){
+    db.transaction(agenda_update_db, errorDB, successDB)
+}
+
+function agenda_update_db(tx){
+    var agenda_id_novo = $("#agenda_id_update").val();
+    var agenda_nome_novo = $("#agenda_nome_update").val();
+    var agenda_telefone_novo = $("#agenda_telefone_update").val();
+    tx.executeSql('UPDATE agenda SET nome = "' + agenda_nome_novo + '", tel = "' + agenda_telefone_novo + '" WHERE id = ' + agenda_id_novo);
+
+    agenda_update_fechar_tela();
+    agenda_view();
 }
